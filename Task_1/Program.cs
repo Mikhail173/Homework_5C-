bb36b253edd30f0aed82aa23efbe539c6327c269
Mikhail173 <2016.Nevermind.2020@gmail.com>
2022-11-11 08:52:55 +0300

    Task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Input the size of the array");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int sum  = 0;
+int[] array = GetArray(size);
+
+int[] GetArray(int size)
+
+{
+    
+    int[] array = new int[size];
+    
+    for (int i=0; i<size; i++)
+    {   
+        array[i]=new Random().Next(100,1000);
+        int element = array[i];
+        if (element % 2 == 0)
+        {
+            sum += 1;
+        }
+        
+    }
+    return array;
+}
+Console.WriteLine(String.Join(",",array));
+Console.WriteLine($"Sum of even numbers: {sum}");
